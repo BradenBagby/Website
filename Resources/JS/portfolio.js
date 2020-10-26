5fbc15fa6c9efd8c6f9f8803b183da37869cbc5e
braden <bradenjbagby@gmail.com>
2020-10-26 12:41:35 -0500

    search filter

--- Resources/JS/portfolio.js
+++ Resources/JS/portfolio.js
@@ -1,4 +1,7 @@
-window.addEventListener('DOMContentLoaded', (event) => {
+
+var skills = {};
+
+window.addEventListener('DOMContentLoaded', async (event) => {
     console.log('DOM fully loaded and parsed');
 
 
@@ -10,32 +13,55 @@ window.addEventListener('DOMContentLoaded', (event) => {
         
     }*/
 
+    
+    await getSkills();
+    await placeSkills();
+
     //initially enable all skills
     enableAllSkills();
 
     //setup click skill listener
-    setupSkillClickListener();
+    setupListeners();
 
 
 });
 
 
 
+///SETUP/////////////////
+async function getSkills(){
+    const response = await fetch('/API/QUERIES/PORTFOLIO/skills');
+    const json = await response.json();
+    skills = json;
+}
 
 
+function placeSkills(){
+    const root = document.getElementById("skillsList");
+    skillsList.innerHTML = "";
+    for (const [key, value] of Object.entries(skills)) {
+        const newSkillElement = skillElement(key);
+        skillsList.appendChild(newSkillElement);
+      }
+}
 
-
-
-////////////////////ENABLE DISABLE SKILLS
-function setupSkillClickListener() {
+function setupListeners() {
     const skills = document.getElementsByClassName("skill");
     for (var i = 0; i < skills.length; i++) {
         const skill = skills[i];
         skill.removeEventListener('click', clickedSkill); //not sure how js would handle du[licate listeners if we ever call this function twice for some reason
         skill.addEventListener('click', clickedSkill);
-
     }
+
+    const search = document.getElementById('skillSearch');
+    search.addEventListener('input', function (evt) {
+        filterSkills(this.value);
+    });
 }
+
+
+
+////////////////////ENABLE DISABLE SKILLS
 
 function clickedSkill(e) {
     const skill = this;
@@ -51,7 +77,9 @@ function enableAllSkills() {
     const skills = document.getElementsByClassName("skill");
     for (var i = 0; i < skills.length; i++) {
         const skill = skills[i];
+        setSkillEnabled(skill,false);
        setSkillEnabled(skill,true);
+       setSkillBig(skill,false);
     }
 }
 
@@ -62,15 +90,71 @@ function setSkillEnabled(skill,enabled){
     skill.classList.add(add);
 
 }
+
+function setSkillBig(skill,big){
+        if(big){
+            skill.classList.add("skill-big");
+        }else{
+            skill.classList.remove("skill-big");
+
+        }
+ 
+   
+}
 /////////////////////////////////////////
 //////////////////SEARCH SKILLS///////////////
 
 function filterSkills(filter){
+    filter = filter.toLowerCase();
+    
  ///start by showing all skills
+ enableAllSkills();
 
  //now if filter is nothing, end
+ if(filter == null || filter == ""){
+     return;
+ }
 
  //otherwise, loop through all skills and if that skill doesnt contain search query then hide it
  //note: database we may want to add tags to each skill to popup in a search
+ for (const [key, value] of Object.entries(skills)) {
+     const originalSkill = key;
+     const skill = String(key).toLocaleLowerCase();
+        if(skill.indexOf(filter) != -1){
+            setSkillBig(document.getElementById(originalSkill),true)
+     
+            continue;
+        }
+
+        if(value != null && value.length > 0){
+            var found = false;
+            for(x of value){
+              
+                x = String(x).toLowerCase();
+                if(x.indexOf(filter) != -1){
+                    found = true;
+                    break;
+                }
+            }
+            if(found){
+                continue;
+            }
+        }
+   
+        setSkillEnabled(document.getElementById(originalSkill),false);
+ }
  //also remove capitalizations
+}
+
+
+//////////////UI CREAATION
+
+function skillElement(skill){
+    const skillElement = document.createElement('div');
+    skillElement.id = skill;
+    skillElement.classList.add("skill");
+    const spanElement = document.createElement("span");
+    spanElement.innerHTML = "" + skill;
+    skillElement.appendChild(spanElement);
+    return skillElement;
 }
