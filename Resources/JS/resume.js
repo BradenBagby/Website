df6ec03e6d0ac5f2d79aa446060295ca0ea9deb3
braden <bradenjbagby@gmail.com>
2020-12-13 01:06:28 -0600

    links

---
+++ Resources/JS/resume.js
@@ -0,0 +1,30 @@
+function blinkLinks() {
+    //fade out all links
+    const links = document.getElementsByTagName('a');
+    for (var i = 0; i < links.length; i++) {
+        const link = links[i];
+        link.style.color = "#007bff";
+        // link.style.textDecoration = "underline";
+    }
+
+    setTimeout(function() {
+        const links = document.getElementsByTagName('a');
+        for (var i = 0; i < links.length; i++) {
+            const link = links[i];
+            link.style.color = "";
+            //link.style.textDecoration = "";
+        }
+    }, 2000);
+}
+
+window.addEventListener('load', (event) => {
+
+
+
+    blinkLinks();
+
+    setInterval(function() {
+        blinkLinks();
+
+    }, 15000);
+});
