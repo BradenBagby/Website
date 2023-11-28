85eb7d1e8e6c80fd665de3cdfec6dba950737197
BradenBagby <bradenjbagby@gmail.com>
2023-11-28 10:34:06 -0700

    darkmode

---
+++ public/Resources/JS/darkmode.js
@@ -0,0 +1,17 @@
+window.addEventListener('load', (event) => {
+	const darkModeSwitch = document.createElement('label');
+	darkModeSwitch.className = 'switch';
+	darkModeSwitch.innerHTML = `
+    <img class="switch-icon" src="Assets/lightbulb-regular.svg">
+      <input type="checkbox" id="dark-mode-toggle">
+      <span class="slider"></span>
+    `;
+
+	// Append the switch to the body
+	document.body.appendChild(darkModeSwitch);
+	const toggleSwitch = document.querySelector('#dark-mode-toggle');
+
+	toggleSwitch.addEventListener('change', function () {
+		document.body.classList.toggle('dark-mode', this.checked);
+	});
+});
--- public/Resources/JS/resume.js
+++ public/Resources/JS/resume.js
@@ -1,30 +1,26 @@
 function blinkLinks() {
-    //fade out all links
-    const links = document.getElementsByTagName('a');
-    for (var i = 0; i < links.length; i++) {
-        const link = links[i];
-        link.style.color = "#007bff";
-        // link.style.textDecoration = "underline";
-    }
+	//fade out all links
+	const links = document.getElementsByTagName('a');
+	for (var i = 0; i < links.length; i++) {
+		const link = links[i];
+		link.style.color = '#007bff';
+		// link.style.textDecoration = "underline";
+	}
 
-    setTimeout(function() {
-        const links = document.getElementsByTagName('a');
-        for (var i = 0; i < links.length; i++) {
-            const link = links[i];
-            link.style.color = "";
-            //link.style.textDecoration = "";
-        }
-    }, 2000);
+	setTimeout(function () {
+		const links = document.getElementsByTagName('a');
+		for (var i = 0; i < links.length; i++) {
+			const link = links[i];
+			link.style.color = 'inherit';
+			//link.style.textDecoration = "";
+		}
+	}, 2000);
 }
 
 window.addEventListener('load', (event) => {
+	blinkLinks();
 
-
-
-    blinkLinks();
-
-    setInterval(function() {
-        blinkLinks();
-
-    }, 15000);
+	setInterval(function () {
+		blinkLinks();
+	}, 15000);
 });
